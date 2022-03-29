72997d99d4f88a7032657d28e12b926f726904c5
renatomak <renato.mark.silva@gmail.com>
2022-03-29 17:16:34 -0300

    checkStyle-02

--- src/main/java/com/trybe/acc/java/Imc.java
+++ src/main/java/com/trybe/acc/java/Imc.java
@@ -11,21 +11,19 @@ public class Imc {
   */
   public static void main(String[] args) {
     // Ler a entrada de um número pelo console	
-	Scanner scan = new Scanner(System.in).useLocale(Locale.US);
-			
-	System.out.println("Digite o peso:");
-	float peso = scan.nextFloat();
-			
-	System.out.println("Digite a aultura:");
-	float altura = scan.nextFloat();
-			
-	// Seu código aqui
-	float imc = peso / (altura * altura);
-			
-					
-	// Imprime a saída no console. OBS: descomente a próxima linha
-	System.out.println(imc);
-
-	}
-
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+		
+    System.out.println("Digite o peso:");
+    float peso = scan.nextFloat();
+		
+    System.out.println("Digite a aultura:");
+    float altura = scan.nextFloat();
+		
+    // Seu código aqui
+    float imc = peso / (altura * altura);
+							
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println(imc);
+  }
+  
 }
