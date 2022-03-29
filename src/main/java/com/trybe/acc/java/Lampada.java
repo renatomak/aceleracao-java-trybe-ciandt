bf05c5554c8c2e9d751b8e8517379f99d9416caa
renatomak <renato.mark.silva@gmail.com>
2022-03-29 17:56:42 -0300

    fist commit

--- src/main/java/com/trybe/acc/java/Lampada.java
+++ src/main/java/com/trybe/acc/java/Lampada.java
@@ -1,13 +1,17 @@
 package com.trybe.acc.java;
 
 public class Lampada {
+  /**
+  * The main method.
+  *
+  * @param args the arguments
+  */
+  public static void main(String[] args) {
+    // Seu código aqui
+    
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    //System.out.println("A lampada ligada: " + isLigada + "!");
 
-	public static void main(String[] args) {
-			// Seu código aqui
-						
-			// Imprime a saída no console. OBS: descomente a próxima linha
-			//System.out.println("A lampada ligada: " + isLigada + "!");
-
-	}
+  }
 
 }
