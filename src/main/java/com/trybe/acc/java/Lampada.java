ee16c3a23ec1b0d159218b707da971ed11843265
renatomak <renato.mark.silva@gmail.com>
2022-03-29 18:02:08 -0300

    challenge finished

--- src/main/java/com/trybe/acc/java/Lampada.java
+++ src/main/java/com/trybe/acc/java/Lampada.java
@@ -8,9 +8,10 @@ public class Lampada {
   */
   public static void main(String[] args) {
     // Seu código aqui
+    boolean isLigada = true;
     
     // Imprime a saída no console. OBS: descomente a próxima linha
-    //System.out.println("A lampada ligada: " + isLigada + "!");
+    System.out.println("A lampada ligada: " + isLigada + "!");
 
   }
 
