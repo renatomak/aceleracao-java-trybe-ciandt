278c2660e0bef78c66ef6b7fde905d034618967d
renatomak <renato.mark.silva@gmail.com>
2022-03-29 19:03:11 -0300

    fist commit

--- src/main/java/com/trybe/acc/java/SomaValores.java
+++ src/main/java/com/trybe/acc/java/SomaValores.java
@@ -4,7 +4,11 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class SomaValores {
-	
+  /**
+  * The main method.
+  *
+  * @param args the arguments
+  */
   public static void main(String[] args) {
     // Ler a entrada de um número pelo console
     Scanner scan = new Scanner(System.in).useLocale(Locale.US);
@@ -23,6 +27,6 @@ public class SomaValores {
     // Imprime a saída no console. OBS: descomente a próxima linha
     //System.out.println("A soma é: " + soma);
 
-  }
-
+ }
+  
 }
