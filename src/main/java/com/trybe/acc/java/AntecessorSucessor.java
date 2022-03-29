f3d7164f9076817e43c5ed244520851e6292f3f9
renatomak <renato.mark.silva@gmail.com>
2022-03-29 06:02:20 -0300

    challenge concluded-eslint-01

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 
 public class AntecessorSucessor {
 
-	public static void main(String[] args) {
-		// Ler a entrada de um número pelo console
-		Scanner scan = new Scanner(System.in);
-
-		System.out.println("Digite um número:");
-		int numero = scan.nextInt();
-
-		int antecessor = numero - 1;
-		int sucessor = numero + 1;
-
-		// Imprime a saída no console. OBS: descomente a próxima linha
-		System.out.println("O número " + numero + " tem " + antecessor + " como seu antecessor e " + sucessor
-				+ " como seu sucessor.");
+  public static void main(String[] args) {
+	  
+	  // Ler a entrada de um número pelo console
+	  Scanner scan = new Scanner(System.in);
+	  
+	  System.out.println("Digite um número:");
+	  int numero = scan.nextInt();
+	  
+	  int antecessor = numero - 1;
+	  int sucessor = numero + 1;
+	  
+	  // Imprime a saída no console. OBS: descomente a próxima linha
+	  System.out.println("O número " + numero + " tem "
+	      + antecessor + " como seu antecessor e "
+          + sucessor + " como seu sucessor."
+      );
 
 	}
 
