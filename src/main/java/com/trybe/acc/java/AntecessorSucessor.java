9c74fd83e527cdf42447b28cde138288e417b778
renatomak <renato.mark.silva@gmail.com>
2022-03-29 15:05:42 -0300

    challenge concluded-checkstyle-spaces

--- src/main/java/com/trybe/acc/java/AntecessorSucessor.java
+++ src/main/java/com/trybe/acc/java/AntecessorSucessor.java
@@ -4,24 +4,28 @@ import java.util.Scanner;
 
 public class AntecessorSucessor {
 
-	/**
-	 * The main method.
-	 *
-	 * @param args the arguments
-	 */
-	public static void main(String[] args) {
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
 
-// Ler a entrant de um numerous pelo console
-		Scanner scan = new Scanner(System.in);
+    // Ler a entrant de um numerous pelo console
+    Scanner scan = new Scanner(System.in);
 
-		System.out.println("Digite um número:");
-		int numero = scan.nextInt();
+    System.out.println("Digite um número:");
+    int numero = scan.nextInt();
 
-		int antecessor = numero - 1;
-		int sucessor = numero + 1;
+    int antecessor = numero - 1;
+    int sucessor = numero + 1;
 
-// Imprime a saída no console. OBS: descomente a próxima linha
-		System.out.println("O número " + numero + " tem " + antecessor + " como seu antecessor e " + sucessor
-				+ " como seu sucessor.");
-	}
+    // Imprime a saída no console. OBS: descomente a próxima linha
+    System.out.println("O número " + numero + " tem " 
+        + antecessor + " como seu antecessor e "
+        + sucessor+ " como seu sucessor."
+    );
+
+  }
+
 }
