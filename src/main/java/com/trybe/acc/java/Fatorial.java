b7b6913a2e0098bd2640d9865cb98c88c4335c0c
renatomak <renato.mark.silva@gmail.com>
2022-03-29 18:51:45 -0300

    challenge finished-checkStyle-02

--- src/main/java/com/trybe/acc/java/Fatorial.java
+++ src/main/java/com/trybe/acc/java/Fatorial.java
@@ -7,25 +7,15 @@ public class Fatorial {
   * @param args the arguments
   */
   public static void main(String[] args) {
+    int index = 2;
+    long fatorial18 = 1;
+    int fat = 18;
+    while (index <= fat) {
+      fatorial18 *= index;
+      index += 1;
+    }
     
-    long fatorial18 = fatorial();
     System.out.println("O fatorial de 18 é igual a " + fatorial18);
 
   }
-  /**
-   * The fatorial method.
-   *
-   * @param 
-   */
-  public static long fatorial() {
-    int index = 2;
-    long result = 1;
-    int fat = 18;
-    while (index <= fat) {
-      result *= index;
-      index += 1;
-    }
-    return result;
-  }
-
 }
