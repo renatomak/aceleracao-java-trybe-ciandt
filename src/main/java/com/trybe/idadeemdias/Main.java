0af615e3575d6223644dc1c87d2b634a5489734a
renatomak <renato.mark.silva@gmail.com>
2022-03-29 23:00:00 -0300

    checkStyle

--- src/main/java/com/trybe/idadeemdias/Main.java
+++ src/main/java/com/trybe/idadeemdias/Main.java
@@ -4,24 +4,23 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
+  /**
+  * The main method.
+  *
+  * @param args the arguments
+  */    
   public static void main(String[] args) {
 
     Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-    String nome = "";
-    int idade = 0;
-    int meses = 0;
-    int dias = 0;
-
     System.out.println("Qual é o seu nome?");
-    nome = scan.next();
+    String nome = scan.next();
     System.out.println("Idade (em anos): ");
-    idade = scan.nextInt();
+    int idade = scan.nextInt();
     System.out.println("meses completos após último aniversário: ");
-    meses = scan.nextInt();
+    int meses = scan.nextInt();
     System.out.println("dia do mês atual: ");
-    dias = scan.nextInt();
+    int dias = scan.nextInt();
 
     Pessoa pessoa = new Pessoa(nome);
 
