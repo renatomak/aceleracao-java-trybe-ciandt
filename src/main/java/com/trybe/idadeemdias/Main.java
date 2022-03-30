b2e1ab9b8f42623740272bc089d0723cf15ae4f8
renatomak <renato.mark.silva@gmail.com>
2022-03-29 22:54:39 -0300

    challenge finished

--- src/main/java/com/trybe/idadeemdias/Main.java
+++ src/main/java/com/trybe/idadeemdias/Main.java
@@ -1,13 +1,33 @@
 package com.trybe.idadeemdias;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+  public static void main(String[] args) {
 
-		
-	}
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+
+    String nome = "";
+    int idade = 0;
+    int meses = 0;
+    int dias = 0;
+
+    System.out.println("Qual é o seu nome?");
+    nome = scan.next();
+    System.out.println("Idade (em anos): ");
+    idade = scan.nextInt();
+    System.out.println("meses completos após último aniversário: ");
+    meses = scan.nextInt();
+    System.out.println("dia do mês atual: ");
+    dias = scan.nextInt();
+
+    Pessoa pessoa = new Pessoa(nome);
+
+    int idadeEmDias = pessoa.calcularIdadeEmDias(idade, meses, dias);
+    System.out.println("Olá " + pessoa.getNome() + ", sua idade em dias é " + idadeEmDias);
+
+  }
 
 }
--- src/main/java/com/trybe/idadeemdias/Pessoa.java
+++ src/main/java/com/trybe/idadeemdias/Pessoa.java
@@ -1,7 +1,23 @@
 package com.trybe.idadeemdias;
 
 public class Pessoa {
-	
 
-	
+  private String nome;
+
+  public int calcularIdadeEmDias(int anos, int meses, int dias) {
+    return (anos * 365) + (meses * 30) + dias;
+  }
+
+  public Pessoa(String nome) {
+    this.nome = nome;
+  }
+
+  public Pessoa() {
+
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
 }
