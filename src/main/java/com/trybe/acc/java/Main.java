a5d1a483ce4b0b9ddf4d2a7e45609a94db8e893c
renatomak <renato.mark.silva@gmail.com>
2022-03-31 07:17:27 -0300

    challenge finished

--- src/main/java/com/trybe/acc/java/Main.java
+++ src/main/java/com/trybe/acc/java/Main.java
@@ -1,10 +1,34 @@
 package com.trybe.acc.java;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+  /**
+   * The main method.
+   *
+   * @param args the arguments
+   */
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in).useLocale(Locale.US);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    System.out.println("Qual é o seu nome? ");
+    String name = scan.next();
 
-	}
+    Nota nota = new Nota();
+
+    double[] assessments = new double[4];
+
+    for (int index = 1; index <= assessments.length; index++) {
+
+      System.out.println("Avaliação nº " + index + ": ");
+      assessments[index - 1] = scan.nextDouble();
+    }
+
+    double avg = nota.calcularMedia(assessments[0], assessments[1], assessments[2], assessments[3]);
+
+    System.out.println("Olá, " + name + ", sua média é " + avg);
+
+  }
 
 }
--- src/main/java/com/trybe/acc/java/Nota.java
+++ src/main/java/com/trybe/acc/java/Nota.java
@@ -1,5 +1,10 @@
 package com.trybe.acc.java;
 
 public class Nota {
+  
+  public double calcularMedia(double gradeOne, double gradeTwo,double gradeThree, double gradeFour) {
+    return (gradeOne + gradeTwo + gradeThree + gradeFour) / 4;
+  }
+  
 
 }
