daae923a7410cd03e247e7d31a73cf7ebf8cabe2
renatomak <renato.mark.silva@gmail.com>
2022-03-31 17:47:31 -0300

    record

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -3,6 +3,7 @@ package com.trybe.acc.java.controledeacesso;
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -15,6 +16,31 @@ public class Principal {
         + "1 - Acessar o estabelecimento\n" + "2 - Finalizar sistema e mostrar relatório");
 
     ArrayList<Short> ages = new ArrayList<Short>();
+//    ages.add((short) 16);
+//    ages.add((short) 16);
+//
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    ages.add((short) 25);
+//    
+//    ages.add((short) 55);
+//    ages.add((short) 55);
+//    ages.add((short) 55);
+
+    
+
     short option = 0;
     do {
       System.out.println(menu);
@@ -25,13 +51,13 @@ public class Principal {
         checkAge(age);
         ages.add(age);
       } else if (option == 2) {
-        System.out.println("Exibir relatrio");
-        imprime(ages);
+        showReport(ages);
       } else {
         System.out.println("Entre com uma opção válida!");
       }
     } while (option != 2);
 
+    showReport(ages);
 
   }
 
@@ -41,12 +67,45 @@ public class Principal {
     }
   }
 
+  static void showReport(ArrayList<Short> ages) {
+    int sizeAges = ages.size();
+
+    int under18 = ages.stream().filter((age) -> age < 18).collect(Collectors.toList()).size();
+    int adults =
+        ages.stream().filter((age) -> age >= 18 && age <= 50).collect(Collectors.toList()).size();
+    int over50 = ages.stream().filter((age) -> age > 50).collect(Collectors.toList()).size();
+
+
+    System.out.println("----- Quantidade -----");
+    System.out.println("menores: " + under18);
+    System.out.println("adultas: " + adults);
+    System.out.println("maiores que 50: " + over50);
+    System.out.println("\n----- Percentual -----");
+    System.out.println("menores: " + formattedPercentage(under18, sizeAges));
+    System.out.println("adultas: " + formattedPercentage(adults, sizeAges));
+    System.out.println("maiores que 50: " + formattedPercentage(over50, sizeAges));
+    System.out.println("\nTOTAL: " + sizeAges);
+
+  }
+
+  static String formattedPercentage(int value, int size) {
+    double percent = ((double) value / size) * 100.00;
+    
+    String percentStr = String.format("%.2f", percent);
+    String subStr = percentStr.substring(percentStr.length()-2);
+    
+    if(subStr.compareTo("00") == 0) {
+      return String.format(Locale.US, "%.1f", percent);
+    }
+    return String.format(Locale.US, "%.2f", percent);
+  }
+
   static void checkAge(Short age) {
     if (age < 18) {
       System.out.println("Pessoa cliente menor de idade, catraca liberada!");
-    } else if (age >= 18 && age <= 50) {
+    } else if (age >= 18 && age < 50) {
       System.out.println("Pessoa adulta, catraca liberada!");
-    } else if (age > 50) {
+    } else if (age >= 50) {
       System.out.println("Pessoa adulta maior de 50, catraca liberada!");
     } else {
       System.out.println("Entre com uma opção válida!");
