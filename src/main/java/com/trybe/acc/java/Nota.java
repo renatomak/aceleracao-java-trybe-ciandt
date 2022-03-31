88dbfadecf8f6ef6cd0bb72d26de40cf9aa16dbb
renatomak <renato.mark.silva@gmail.com>
2022-03-31 07:21:20 -0300

    challenge finished-CheckStyle

--- src/main/java/com/trybe/acc/java/Nota.java
+++ src/main/java/com/trybe/acc/java/Nota.java
@@ -1,10 +1,11 @@
 package com.trybe.acc.java;
 
 public class Nota {
-  
-  public double calcularMedia(double gradeOne, double gradeTwo,double gradeThree, double gradeFour) {
+
+  public double calcularMedia(double gradeOne, double gradeTwo, double gradeThree,
+      double gradeFour) {
     return (gradeOne + gradeTwo + gradeThree + gradeFour) / 4;
   }
-  
+
 
 }
