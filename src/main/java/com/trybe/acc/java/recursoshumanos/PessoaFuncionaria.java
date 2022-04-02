fe3d7828bc842e6277bcdf817474aef26d0a76b3
renatomak <renato.mark.silva@gmail.com>
2022-04-01 22:32:45 -0300

    first commit

--- src/main/java/com/trybe/acc/java/recursoshumanos/PessoaFuncionaria.java
+++ src/main/java/com/trybe/acc/java/recursoshumanos/PessoaFuncionaria.java
@@ -5,4 +5,45 @@ package com.trybe.acc.java.recursoshumanos;
  */
 public class PessoaFuncionaria {
 
+  private String nomeCompleto;
+  private String cpf;
+  private String endereco;
+  private double salario;
+
+  public PessoaFuncionaria(String nomeCompleto, String cpf, String endereco, double salario) {
+    super();
+    this.nomeCompleto = nomeCompleto;
+    this.cpf = cpf;
+    this.endereco = endereco;
+    this.salario = salario;
+  }
+
+  public String getNomeCompleto() {
+    return nomeCompleto;
+  }
+
+  public void setNomeCompleto(String nomeCompleto) {
+    this.nomeCompleto = nomeCompleto;
+  }
+
+  public String getEndereco() {
+    return endereco;
+  }
+
+  public void setEndereco(String endereco) {
+    this.endereco = endereco;
+  }
+
+  public double getSalario() {
+    return salario;
+  }
+
+  public void setSalario(double salario) {
+    this.salario = salario;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
 }
