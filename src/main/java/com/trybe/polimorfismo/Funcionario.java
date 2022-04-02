fa62dca080deff92de81ded0548109da6d9c2a62
renatomak <renato.mark.silva@gmail.com>
2022-04-02 08:26:21 -0300

    first commit

--- src/main/java/com/trybe/polimorfismo/Funcionario.java
+++ src/main/java/com/trybe/polimorfismo/Funcionario.java
@@ -2,5 +2,35 @@ package com.trybe.polimorfismo;
 
 public abstract class Funcionario {
 
+  private String name;
+  private String cpf;
+  private double salarioBruto;
+
+  public abstract double calcularSalarioLiquido();
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public double getSalarioBruto() {
+    return salarioBruto;
+  }
+
+  public void setSalarioBruto(double salarioBruto) {
+    this.salarioBruto = salarioBruto;
+  }
+
 
 }
--- src/main/java/com/trybe/polimorfismo/FuncionarioPessoaJuridica.java
+++ src/main/java/com/trybe/polimorfismo/FuncionarioPessoaJuridica.java
@@ -2,4 +2,10 @@ package com.trybe.polimorfismo;
 
 public class FuncionarioPessoaJuridica extends Funcionario {
 
+  @Override
+  public double calcularSalarioLiquido() {
+    // TODO Auto-generated method stub
+    return this.getSalarioBruto() - (this.getSalarioBruto() * 0.1);
+  }
+
 }
