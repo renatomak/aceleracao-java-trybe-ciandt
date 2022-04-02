2cc01154a1acf414400f924962f9837cde092ad4
renatomak <renato.mark.silva@gmail.com>
2022-04-02 06:51:42 -0300

    checkstyle

--- src/main/java/com/trybe/gerarpessoausuaria/Principal.java
+++ src/main/java/com/trybe/gerarpessoausuaria/Principal.java
@@ -2,11 +2,11 @@ package com.trybe.gerarpessoausuaria;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Usuario p = new Usuario(null, "wayne");
-		System.out.println(p.getUsuario());
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Usuario p = new Usuario(null, "wayne");
+    System.out.println(p.getUsuario());
 
-	}
+  }
 
 }
--- src/main/java/com/trybe/gerarpessoausuaria/Usuario.java
+++ src/main/java/com/trybe/gerarpessoausuaria/Usuario.java
@@ -2,22 +2,22 @@ package com.trybe.gerarpessoausuaria;
 
 public class Usuario extends Pessoa {
 
-	public Usuario(String nome, String sobrenome) {
-		super(nome, sobrenome);
-	}
+  public Usuario(String nome, String sobrenome) {
+    super(nome, sobrenome);
+  }
 
-	/**
-	 * Método para pegar o nome e sobrenome do usuário.
-	 *
-	 */
-	public String getUsuario() {
-		String nome = super.nome;
-		String sobrenome = super.sobrenome;
+  /**
+   * Método para pegar o nome e sobrenome do usuário.
+   *
+   */
+  public String getUsuario() {
+    String nome = super.nome;
+    String sobrenome = super.sobrenome;
 
-		if (nome != null && nome.trim() != "" && sobrenome != null && sobrenome.trim() != "") {
-			return String.format("%s.%s", nome, sobrenome);
-		}
-		return "Usuário inválido";
+    if (nome != null && nome.trim() != "" && sobrenome != null && sobrenome.trim() != "") {
+      return String.format("%s.%s", nome, sobrenome);
+    }
+    return "Usuário inválido";
 
-	}
+  }
 }
