eb3fc0063f3066810b4f68f00d8e223bb88ab485
renatomak <renato.mark.silva@gmail.com>
2022-04-01 22:25:03 -0300

    checkStyle

--- src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
@@ -2,28 +2,28 @@ package com.trybe.contapoupanca;
 
 public class ContaPoupanca {
 
-    private double saldo;
-    private String titularConta;
+  private double saldo;
+  private String titularConta;
 
-    public ContaPoupanca(String titularConta, double saldo) {
-        this.saldo = saldo;
-        this.titularConta = titularConta;
-    }
+  public ContaPoupanca(String titularConta, double saldo) {
+    this.saldo = saldo;
+    this.titularConta = titularConta;
+  }
 
-    public void depositar(double value) {
-        this.saldo += value;
-    }
+  public void depositar(double value) {
+    this.saldo += value;
+  }
 
-    public void sacar(double value) {
-        this.saldo -= value;
-    }
+  public void sacar(double value) {
+    this.saldo -= value;
+  }
 
-    public double mostrarSaldo() {
-        return this.saldo;
-    }
+  public double mostrarSaldo() {
+    return this.saldo;
+  }
 
-    public String mostrarTitularConta() {
-        return this.titularConta;
-    }
+  public String mostrarTitularConta() {
+    return this.titularConta;
+  }
 
 }
