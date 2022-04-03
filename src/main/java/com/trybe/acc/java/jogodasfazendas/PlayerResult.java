5846b63c55349f8e5dae7220d2b1370761002026
renatomak <renato.mark.silva@gmail.com>
2022-04-03 11:40:38 -0300

    first commit

--- src/main/java/com/trybe/acc/java/jogodasfazendas/PlayerResult.java
+++ src/main/java/com/trybe/acc/java/jogodasfazendas/PlayerResult.java
@@ -1,7 +1,32 @@
 package com.trybe.acc.java.jogodasfazendas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlayerResult {
 
+  private String playerName;
+  private Farm[] farms;
 
+  public PlayerResult(String playerName, Farm[] farms) {
+    super();
+    this.playerName = playerName;
+    this.farms = farms;
+  }
+
+  public double score() {
+    double sum = 0;
+
+    for (int i = 0; i < farms.length; i++) {
+      sum += farms[i].area();
+    }
+    return sum;
+  }
+
+  @Override
+  public String toString() {
+    return "PlayerResult [playerName=" + playerName + ", score()=" + score() + ", getClass()="
+        + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+  }
 
 }
---
+++ src/main/java/com/trybe/acc/java/jogodasfazendas/Polygon.java
@@ -0,0 +1,23 @@
+package com.trybe.acc.java.jogodasfazendas;
+
+public abstract class Polygon implements Farm {
+
+  protected double base;
+  protected double height;
+
+
+
+  public Polygon(double base, double height) {
+    super();
+    this.base = base;
+    this.height = height;
+  }
+
+
+  @Override
+  public double area() {
+    // TODO Auto-generated method stub
+    return base * height;
+  }
+
+}
