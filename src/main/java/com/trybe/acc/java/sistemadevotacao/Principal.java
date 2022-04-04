cbbd5659002274ce34a85755e45cf02ee76ca31a
Renato Marques <32239996+renatomak@users.noreply.github.com>
2022-04-04 06:26:36 -0300

    refact

--- src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/Principal.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 
 public class Principal {
 
+  public static final NOME_PESSOA = "Entre com o nome da pessoa ";
+  public static final NUMERO_PESSOA = "Entre com o número da pessoa candidata: "
+  public static final CPF_PESSOA = "Entre com o cpf da pessoa "
+  public static final CANDIDATA = "candidata: "
+  public static final ELEITORA = "eleitora: "
+
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     Scanner scan = new Scanner(System.in).useLocale(Locale.US);
@@ -15,16 +21,16 @@ public class Principal {
     String nome = "";
     Integer numero = 0;
     String cpf = "";
-    short opcao = -1;
+    short opcao = 0;
 
     do {
       utils.menuCadastroPessoasCandidatas();
       opcao = scan.nextShort();
 
       if(opcao == 1) {
-        imprimir("Entre com o nome da pessoa candidata: ");
+        utils.imprimir(NOME_PESSOA + CANDIDATA);
         nome = scan.next();
-        imprimir("Entre com o número da pessoa candidata: ");
+        utils.imprimir(NUMERO_PESSOA + CANDIDATA);
         numero = scan.nextInt();
         gerenciamentoVotacao.cadastrarPessoaCandidata(nome, numero);
       }
@@ -33,15 +39,15 @@ public class Principal {
     
     // gerenciamentoVotacao.mostrarCandidatos();
     
-    opcao = -1;
+    opcao = 0;
     do {
       utils.menuCadastroPessoasEleitoras();
       opcao = scan.nextShort();
       
       if (opcao == 1) {
-        imprimir("Entre com o nome da pessoa eleitora: ");
+        utils.imprimir(NOME_PESSOA + ELEITORA);
         nome = scan.next();
-        imprimir("Entre com o cpf da pessoa eleitora: ");
+        utils.imprimir(NUMERO_PESSOA + ELEITORA);
         cpf = scan.next();
       
         gerenciamentoVotacao.cadastrarPessoaEleitora(nome, cpf);
@@ -49,17 +55,17 @@ public class Principal {
     } while(opcao == 1)
     
     // gerenciamentoVotacao.mostrarEleitores();
-    opcao = -1;
+    opcao = 0;
     do{
       utils.menuVotacao();
       opcao = scan.nextShort();
 
       switch (opcao) {
         case 1:
-          utils.imprimir("Entre com o cpf da pessoa eleitora: ");
+          utils.imprimir(CPF_PESSOA + ELEITORA);
           cpf = scan.next();
 
-          utils.imprimir("Entre com o número da pessoa candidata: ");
+          utils.imprimir(NUMERO_PESSOA + CANDIDATA);
           numero = scan.nextInt();
           gerenciamentoVotacao.votar(cpf, numero);
           break;
