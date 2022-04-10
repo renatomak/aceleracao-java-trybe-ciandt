0658b26e67f98e702304937b90a0043fc5887556
Renato Marques <32239996+renatomak@users.noreply.github.com>
2022-04-10 19:25:25 -0300

    First commit

---
+++ src/main/java/com/trybe/acc/java/transformatexto/Main.java
@@ -0,0 +1,13 @@
+package com.trybe.acc.java.transformatexto;
+
+public class Main {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Texto texto = new Texto();
+    String teste = "Lorem+ipsum+dolort+sit+ammet";
+    System.out.println(texto.transformaTexto(teste));
+  }
+  
+
+}
--- src/main/java/com/trybe/acc/java/transformatexto/Texto.java
+++ src/main/java/com/trybe/acc/java/transformatexto/Texto.java
@@ -2,4 +2,17 @@ package com.trybe.acc.java.transformatexto;
 
 public class Texto {
 
+  public String transformaTexto(String string) {
+    String newString = string.replace("+", " ");
+    int indexA = string.indexOf("a");
+
+    if (indexA % 2 == 0) {
+      newString = newString.toUpperCase();
+    } else {
+      String[] array = newString.split(" ");
+      newString = array[0];
+    }
+    return newString;
+  }
+
 }
