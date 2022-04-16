94b4a79f0a7d18504b6ae6212231c948768ee48c
renatomak <renato.mark.silva@gmail.com>
2022-04-16 14:02:40 -0300

    first commit

--- src/main/java/com/trybe/acc/java/Divisor.java
+++ src/main/java/com/trybe/acc/java/Divisor.java
@@ -2,4 +2,15 @@ package com.trybe.acc.java;
 
 public class Divisor {
 
+  public Integer dividir(int a, int b) {
+    int result = 0;
+    try {
+      result = a / b;
+    } catch (ArithmeticException e) {
+      // TODO: handle exception
+      System.out.println("Não é possível fazer divisão por zero.");
+    }
+    return result;
+  }
+
 }
