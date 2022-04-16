905a0b1c36e4fa2cdfd16e12c0acaf0d9c376fa6
renatomak <renato.mark.silva@gmail.com>
2022-04-16 14:50:46 -0300

    first commit

--- src/main/java/com/trybe/acc/java/NullGenerator.java
+++ src/main/java/com/trybe/acc/java/NullGenerator.java
@@ -2,4 +2,18 @@ package com.trybe.acc.java;
 
 public class NullGenerator {
 
+  /**
+   * getName return String.
+   * 
+   * @param number integer
+   * @return string
+   * @throws NullPointerException
+   */
+  public String getNome(Integer number) throws NullPointerException {
+    if (number % 2 != 0) {
+      throw new NullPointerException();
+    }
+    return "BOB";
+  }
+
 }
