5631c2177d8cd13d5c4a42428984ae4ac603b576
Renato Marques <32239996+renatomak@users.noreply.github.com>
2022-04-30 22:09:40 -0300

    first commit

--- src/main/java/com/trybe/acc/java/Agenda.java
+++ src/main/java/com/trybe/acc/java/Agenda.java
@@ -4,7 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Agenda {
-	private List<String> contatos = new ArrayList<>();
-	
-	
+  private List<String> contatos = new ArrayList<>();
+
+  void incluirContato(String contato) {
+    contatos.add(contato);
+  }
+
+  void excluirContato(String contato) {
+    contatos.remove(contato);
+  }
+
+  boolean verificaContato(String contato) {
+    return contatos.contains(contato);
+  }
+
+  int tamanhoAgenda() {
+    return contatos.size();
+  }
+
 }
