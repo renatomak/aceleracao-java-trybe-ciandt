05b9f04f6f25b654985463c054b6f82f5492b105
renatomak <renato.mark.silva@gmail.com>
2022-05-02 10:02:06 -0300

    first commit

--- src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
+++ src/main/java/com/trybe/acc/java/OperacoesVeiculos.java
@@ -1,5 +1,21 @@
 package com.trybe.acc.java;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class OperacoesVeiculos {
+
+  public <T> int numVeiculosDiferentesVendidos(Stream<T> veiculosVendidos) {
+    return (int) veiculosVendidos.distinct().count();
+  }
+
+  public <T> Stream<String> obterRepresentacao(Stream<T> veiculos) {
+    return veiculos.map(item -> item.toString());
+  }
+
+  public <T> List<T> obterLista(Stream<T> veiculos) {
+    return veiculos.collect(Collectors.toList());
+  }
 
 }
