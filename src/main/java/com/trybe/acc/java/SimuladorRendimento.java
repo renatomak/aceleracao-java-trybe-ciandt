900dc1ad23aea3b7bb0a8ce6a6da02371a263524
renatomak <renato.mark.silva@gmail.com>
2022-05-05 17:35:42 -0300

    first commit

--- src/main/java/com/trybe/acc/java/SimuladorRendimento.java
+++ src/main/java/com/trybe/acc/java/SimuladorRendimento.java
@@ -1,6 +1,32 @@
 package com.trybe.acc.java;
 
+import java.time.LocalDateTime;
 
 public class SimuladorRendimento {
+
+  public static void main(String[] args) {
+    SimuladorRendimento obj = new SimuladorRendimento();
+    double inicial = 500;
+    double objetivo = 5000;
+    System.out.println("Com " + inicial + " seu rendimento será " + objetivo + " no ano "
+        + obj.simularRendimentoAnos(inicial, objetivo));
+  }
+
+  public int simularRendimentoAnos(double initial, double objective) {
+    double yieldRate = 0.10;
+    double yield = initial;
+    int countYear = 0;
+
+    do {
+      yield += yield * yieldRate;
+      countYear++;
+
+    } while (yield < objective);
+
+    LocalDateTime resultYears = LocalDateTime.now().plusYears(countYear);
+
+
+    return resultYears.getYear();
+  }
 
 }
