9149d5d8697ea44e4a86a552a2ed79b2ec6e8ab7
renatomak <renato.mark.silva@gmail.com>
2022-05-07 20:59:47 -0300

    first commit

--- src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
@@ -1,10 +1,13 @@
 package com.betrybe.acc.java.bankaccount;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * AccountNumberFormatter.
  */
 public class AccountNumberFormatter {
+  private static final int DEFAULT_SIZE_ACCOUNT_NUMBER = 6;
 
   /**
    * Formata o número da conta.
@@ -12,7 +15,15 @@ public class AccountNumberFormatter {
    * @param literalAccountNumber número da conta
    */
   public String formatAccountNumber(int literalAccountNumber) {
-
+    String str = "" + literalAccountNumber;
+    if (str.length() == DEFAULT_SIZE_ACCOUNT_NUMBER) {
+      return str;
+    }
+    if (str.length() < DEFAULT_SIZE_ACCOUNT_NUMBER) {
+      return StringUtils.leftPad(str, DEFAULT_SIZE_ACCOUNT_NUMBER, '0');
+    } else {
+      return StringUtils.right(str, DEFAULT_SIZE_ACCOUNT_NUMBER);
+    }
   }
 
 }
--- src/main/java/com/betrybe/acc/java/bankaccount/Application.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/Application.java
@@ -1,5 +1,7 @@
 package com.betrybe.acc.java.bankaccount;
 
+import java.util.Scanner;
+
 /**
  * A class.
  */
@@ -9,7 +11,16 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Informe o número da conta: ");
+    int accountNumber = scanner.nextInt();
+    scanner.nextLine();
 
+    AccountNumberFormatter accountNumberFormatter = new AccountNumberFormatter();
+
+    String result = accountNumberFormatter.formatAccountNumber(accountNumber);
+
+    System.out.println("Número da conta: " + result);
   }
 
 }
