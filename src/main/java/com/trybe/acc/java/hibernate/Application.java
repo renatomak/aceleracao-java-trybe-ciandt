8a46f97d4f272796e994a9a27ee363a9bda42893
renatomak <renato.mark.silva@gmail.com>
2022-05-13 19:30:33 -0300

    first commit

--- src/main/java/com/trybe/acc/java/hibernate/Application.java
+++ src/main/java/com/trybe/acc/java/hibernate/Application.java
@@ -1,6 +1,14 @@
 package com.trybe.acc.java.hibernate;
 
+import com.trybe.acc.java.hibernate.entity.Veiculo;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Application {
+  static EntityManagerFactory emf =
+      Persistence.createEntityManagerFactory("com.trybe.acc.java.hibernate.veiculo-hibernate");
 
   /**
    * Método principal.
@@ -8,6 +16,18 @@ public class Application {
    * @param args argumentos da linha de comando
    */
   public static void main(String[] args) {
+    Veiculo veiculo = new Veiculo();
+    veiculo.setAno(2022);
+    veiculo.setModelo("Esportivo");
+    veiculo.setQuilometragem("50Km");
+
+    EntityManager em = emf.createEntityManager();
+
+    em.getTransaction().begin();
+    em.persist(veiculo);
+    em.getTransaction().commit();
+
+    em.close();
 
 
   }
