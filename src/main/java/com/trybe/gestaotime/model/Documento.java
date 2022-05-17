08276df2cb76b190ae87c76e969b7778275b43a5
renatomak <renato.mark.silva@gmail.com>
2022-05-17 17:00:13 -0300

    feat: entity documento

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -5,9 +5,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-/**
- * Classe Documento.
- */
 
 @Entity
 public class Documento {
