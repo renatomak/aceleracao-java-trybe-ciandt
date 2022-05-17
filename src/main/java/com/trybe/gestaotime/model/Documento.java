ac12741c185712b203843d09fc04f0e98fe33171
renatomak <renato.mark.silva@gmail.com>
2022-05-17 16:59:58 -0300

    feat: entity documento

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -1,16 +1,52 @@
 package com.trybe.gestaotime.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Classe Documento.
  */
 
+@Entity
 public class Documento {
-  /**
-   * Atributos.
-   */
 
-  /**
-   * Métodos.
-   */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String cpf;
+
+    private String numeroCarteiraTrabalho;
+
+    private String numeroCbf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumeroCarteiraTrabalho() {
+        return numeroCarteiraTrabalho;
+    }
+
+    public void setNumeroCarteiraTrabalho(String umeroCarteiraTrabalho) {
+        this.numeroCarteiraTrabalho = umeroCarteiraTrabalho;
+    }
+
+    public String getNumeroCbf() {
+        return numeroCbf;
+    }
+
+    public void setNumeroCbf(String numeroCbf) {
+        this.numeroCbf = numeroCbf;
+    }
 }
