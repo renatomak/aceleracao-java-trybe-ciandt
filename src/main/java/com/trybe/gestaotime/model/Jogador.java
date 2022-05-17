f0db99d22198107af8817bcdea3d4baf65a7ffa1
renatomak <renato.mark.silva@gmail.com>
2022-05-17 17:15:30 -0300

    feat: entity jogador

--- src/main/java/com/trybe/gestaotime/model/Jogador.java
+++ src/main/java/com/trybe/gestaotime/model/Jogador.java
@@ -1,17 +1,66 @@
 package com.trybe.gestaotime.model;
 
-/**
- * Classe Jogador.
- **/
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
+@Entity
 public class Jogador {
 
-  /**
-   * Atributos.
-   **/
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  /**
-   * Métodos.
-   **/
+    private String nome;
 
+    private String posicao;
+
+    @OneToOne(mappedBy = "jogador", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Documento documento;
+
+    @ManyToOne
+    @JoinColumn(name = "time_id")
+    private Time time;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public Documento getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(Documento documento) {
+        this.documento = documento;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
 }
