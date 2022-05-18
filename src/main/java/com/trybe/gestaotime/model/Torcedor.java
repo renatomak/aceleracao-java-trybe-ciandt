e0dfb18c60fd6bbbbe67e005eceb1e5257bf36f9
renatomak <renato.mark.silva@gmail.com>
2022-05-18 08:31:16 -0300

    feat: entity Torcedor

--- src/main/java/com/trybe/gestaotime/model/Torcedor.java
+++ src/main/java/com/trybe/gestaotime/model/Torcedor.java
@@ -9,11 +9,31 @@ public class Torcedor {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
     private String nome;
 
     @ManyToMany(mappedBy = "torcedores")
     private List<Time> times = new ArrayList<>();
 
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Time> getTimes() {
+        return times;
+    }
+
+    public void setTimes(List<Time> times) {
+        this.times = times;
+    }
 }
