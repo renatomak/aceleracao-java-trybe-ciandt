e92617d51042b16dfdc7d7a05042be35d5fb4faf
renatomak <renato.mark.silva@gmail.com>
2022-05-18 08:30:51 -0300

    feat: init DocumentoDao

--- src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
+++ src/main/java/com/trybe/gestaotime/dao/DocumentoDao.java
@@ -2,6 +2,33 @@ package com.trybe.gestaotime.dao;
 
 import com.trybe.gestaotime.model.Documento;
 
+import java.util.List;
+
 public class DocumentoDao extends GenericDao<Documento, Integer> {
+
+    @Override
+    public void salvar(Documento entity) {
+        super.salvar(entity);
+    }
+
+    @Override
+    public void editar(Documento entity) {
+        super.editar(entity);
+    }
+
+    @Override
+    public void deletar(Integer id) {
+    }
+
+    @Override
+    public List<Documento> listar() {
+        return super.listar();
+    }
+
+    @Override
+    public Documento findById(Long id) {
+        return null;
+    }
+
 
 }
