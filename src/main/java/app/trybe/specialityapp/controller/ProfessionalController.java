d003f59260360add51f02be1e4ad4d4b475a353e
renatomak <renato.mark.silva@gmail.com>
2022-05-27 18:24:29 -0300

    feat: application error

--- src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
+++ src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
@@ -15,18 +15,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 
 
 @RestController
-@RequestMapping("/professionals")
+@RequestMapping("/professional")
 public class ProfessionalController {
 
   @Autowired
   private ProfessionalService professionalService;
 
-  @GetMapping
+  @GetMapping(value = "/all")
   public ResponseEntity<List<Professional>> findAll() {
     List<Professional> professionals = professionalService.findAll();
     return ResponseEntity.ok(professionals);
@@ -43,19 +41,19 @@ public class ProfessionalController {
    * @param professional type Professional.
    * @return ResponseEntity type Professional.
    */
-  @PostMapping("/add")
+  @PostMapping(value = "/add")
   public ResponseEntity<Professional> insert(@RequestBody Professional professional) {
     professional = professionalService.insert(professional);
     return ResponseEntity.status(HttpStatus.CREATED).body(professional);
   }
 
-  @PutMapping(value = "/{id}")
-  public ResponseEntity<Professional> update(@PathVariable Integer id,
+  @PutMapping(value = "/edit/{id}")
+  public ResponseEntity<Professional> edit(@PathVariable Integer id,
       @RequestBody Professional professional) {
     return ResponseEntity.ok(professionalService.edit(id, professional));
   }
 
-  @DeleteMapping(value = "/{id}")
+  @DeleteMapping(value = "/delete/{id}")
   public ResponseEntity<Void> delete(@PathVariable Integer id) {
     professionalService.delete(id);
     return ResponseEntity.noContent().build();
