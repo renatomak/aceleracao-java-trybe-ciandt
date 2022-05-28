41114470d6f0615460efbc78e38a1f0f914f91a6
renatomak <renato.mark.silva@gmail.com>
2022-05-28 18:06:33 -0300

    refact: JerseyConfig

--- src/main/java/app/trybe/specialityapp/config/JerseyConfig.java
+++ src/main/java/app/trybe/specialityapp/config/JerseyConfig.java
@@ -1,33 +1,14 @@
 package app.trybe.specialityapp.config;
 
-import app.trybe.specialityapp.SpecialityAppApplication;
-import javax.ws.rs.ApplicationPath;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import app.trybe.specialityapp.controller.ProfessionalController;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-
-@Configuration
-@ApplicationPath("/api")
+@Component
 public class JerseyConfig extends ResourceConfig {
 
-  private Logger logger = LoggerFactory.getLogger(JerseyConfig.class);
-
-
-  /**
-   * Construtor para o JerseyConfig.
-   *
-   */
   public JerseyConfig() {
-    packages(SpecialityAppApplication.class.getPackageName());
-    register(new AbstractBinder() {
-
-      @Override
-      protected void configure() {
-        logger.info("Configurando binder");
-      }
-    });
+    register(ProfessionalController.class);
   }
+
 }
