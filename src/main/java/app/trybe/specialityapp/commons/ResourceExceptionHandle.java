9e4daa412a1e4bcbb907e6a00ba7c7676e6c2072
renatomak <renato.mark.silva@gmail.com>
2022-05-29 10:14:50 -0300

    refact: type status

--- src/main/java/app/trybe/specialityapp/commons/ResourceExceptionHandle.java
+++ src/main/java/app/trybe/specialityapp/commons/ResourceExceptionHandle.java
@@ -6,20 +6,19 @@ import javax.ws.rs.core.Response;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-
 @ControllerAdvice
 public class ResourceExceptionHandle {
 
   /**
    * Method controller advice.
-   * 
+   *
    * @param e ResourceNotFoundException.
    * @param request HttpServletRequest.
    * @return ResponseEntity.
    */
   @ExceptionHandler(ResourceNotFoundException.class)
   public Response entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-    ApplicationError err = new ApplicationError(404, e.getMessage());
+    ApplicationError err = new ApplicationError(Response.Status.NOT_FOUND, e.getMessage());
 
     return Response.status(Response.Status.NOT_FOUND).entity(err).build();
   }
