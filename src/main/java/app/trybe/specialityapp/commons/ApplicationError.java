775f3a5f7cf034a07706702d557a50471e7d290a
renatomak <renato.mark.silva@gmail.com>
2022-05-29 10:11:19 -0300

    feat: creat messages

--- src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
+++ src/main/java/app/trybe/specialityapp/commons/ApplicationError.java
@@ -1,20 +1,22 @@
 package app.trybe.specialityapp.commons;
 
+import javax.ws.rs.core.Response;
+
 public class ApplicationError {
 
-  private Integer status;
+  private Response.Status status;
   private String message;
 
-  public ApplicationError(Integer status, String message) {
+  public ApplicationError(Response.Status status, String message) {
     this.status = status;
     this.message = message;
   }
 
-  public Integer getStatus() {
+  public Response.Status getStatus() {
     return status;
   }
 
-  public void setStatus(Integer status) {
+  public void setStatus(Response.Status status) {
     this.status = status;
   }
 
