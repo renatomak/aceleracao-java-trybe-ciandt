bd3d1cbd78a77b2bdf7f26e1a10a58f4c7850447
renatomak <renato.mark.silva@gmail.com>
2022-05-29 10:13:56 -0300

    feat: exception handling

--- src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
+++ src/main/java/app/trybe/specialityapp/controller/ProfessionalController.java
@@ -4,9 +4,13 @@ import static app.trybe.specialityapp.commons.ResponseMessage.deleteMessageSucce
 import static app.trybe.specialityapp.commons.ResponseMessage.editMessageSuccess;
 import static app.trybe.specialityapp.commons.ResponseMessage.insertMessageSuccess;
 
+import app.trybe.specialityapp.commons.ApplicationError;
+import app.trybe.specialityapp.commons.ResponseMessage;
 import app.trybe.specialityapp.model.Professional;
 import app.trybe.specialityapp.service.ProfessionalService;
+import app.trybe.specialityapp.service.exceptions.ResourceNotFoundException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -16,9 +20,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Controller;
 
 
 @Controller
@@ -29,12 +34,23 @@ public class ProfessionalController {
   private ProfessionalService professionalService;
 
 
+  /**
+   * Method findAll Professional.
+   *
+   * @return ResponseEntity type Professional.
+   */
   @GET
   @Path("/all")
   @Produces("application/json")
   public Response findAll() {
-    List<Professional> professionals = professionalService.findAll();
-    return Response.ok(professionals).build();
+    try {
+      List<Professional> professionals = professionalService.findAll();
+      return Response.ok(professionals).build();
+    } catch (ResourceNotFoundException e) {
+      ApplicationError err = new ApplicationError(Response.Status.NOT_FOUND, e.getMessage());
+      return Response.status(Response.Status.NOT_FOUND).entity(err).build();
+    }
+
   }
 
   @GET
@@ -55,25 +71,56 @@ public class ProfessionalController {
   @Consumes("application/json")
   @Produces("application/json")
   public Response insert(Professional professional) {
-    professionalService.insert(professional);
-    return Response.status(Response.Status.CREATED).entity(insertMessageSuccess).build();
+    try {
+      professionalService.insert(professional);
+      return Response.status(Response.Status.CREATED).entity(insertMessageSuccess).build();
+    } catch (ResourceNotFoundException e) {
+      ApplicationError err =
+          new ApplicationError(Response.Status.BAD_REQUEST, ResponseMessage.mensageErroExplicitId);
+      return Response.status(Response.Status.BAD_REQUEST).entity(err).build();
+    }
   }
 
+  /**
+   * Method edit exist Professional.
+   * 
+   * @param id type Integer.
+   * @param professional type Professional.
+   * @return ResponseEntity type Professional.
+   */
   @PUT
   @Path("/edit/{id}")
   @Consumes("application/json")
   @Produces("application/json")
   public Response edit(@PathParam("id") Integer id, Professional professional) {
-    professionalService.edit(id, professional);
-    return Response.ok(editMessageSuccess(id)).build();
+    try {
+      professionalService.edit(id, professional);
+      return Response.ok(editMessageSuccess(id)).build();
+    } catch (NoSuchElementException e) {
+      ApplicationError err = new ApplicationError(Response.Status.NOT_FOUND,
+          ResponseMessage.mensageErroEditDoNotExist);
+      return Response.status(Response.Status.NOT_FOUND).entity(err).build();
+    }
   }
-
+  
+  /**
+   * Method delete Professional.
+   *
+   * @param id type Integer.
+   * @return Response type response.
+   */
   @DELETE
   @Path("/delete/{id}")
   @Produces("application/json")
   public Response delete(@PathParam("id") Integer id) {
-    professionalService.delete(id);
-    return Response.ok(deleteMessageSuccess(id)).build();
+    try {
+      professionalService.delete(id);
+      return Response.ok(deleteMessageSuccess(id)).build();
+    } catch (EmptyResultDataAccessException e) {
+      ApplicationError err = new ApplicationError(Response.Status.NOT_FOUND,
+          ResponseMessage.mensageErroDeleteDoNotExist);
+      return Response.status(Response.Status.NOT_FOUND).entity(err).build();
+    }
   }
 
 }
