f0f803fb076e617e62a80b7aab8703d3c5d28467
renatomak <renato.mark.silva@gmail.com>
2022-05-29 10:15:06 -0300

    feat: messages

--- src/main/java/app/trybe/specialityapp/commons/ResponseMessage.java
+++ src/main/java/app/trybe/specialityapp/commons/ResponseMessage.java
@@ -3,8 +3,12 @@ package app.trybe.specialityapp.commons;
 public class ResponseMessage {
 
   public static final String insertMessageSuccess = "Inserido";
-  public static final String mensagemErroExplicitId = 
+  public static final String mensageErroExplicitId =
       "Não é permitido inserir novos registros com ID explícito";
+  public static final String mensageErroEditDoNotExist =
+          "Não é possível editar, o ID informado não existe";
+  public static final String mensageErroDeleteDoNotExist =
+          "Não é possível deletar, o ID informado não existe";
 
   public static String editMessageSuccess(Integer id) {
     return String.format("ID [%d] atualizado", id);
