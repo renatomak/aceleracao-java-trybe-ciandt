7dfe548498abd3fcd15020892625c5599af9b603
renatomak <renato.mark.silva@gmail.com>
2022-05-30 15:37:20 -0300

    feat: estudante repository

--- src/main/java/com/trybe/camadas/projeto/spring/persistence/repository/EstudanteRepository.java
+++ src/main/java/com/trybe/camadas/projeto/spring/persistence/repository/EstudanteRepository.java
@@ -1,5 +1,17 @@
 package com.trybe.camadas.projeto.spring.persistence.repository;
 
+import com.trybe.camadas.projeto.spring.persistence.entity.Estudante;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface EstudanteRepository {
+
+  Estudante criarEstudante(Estudante estudante);
+
+  Estudante buscaEstudantepeloId(String id);
+
+  Estudante atualizaEstudantepeloId(String id, Estudante estudante);
+
+  void excluiEstudantepeloId(String id);
 
 }
--- src/main/java/com/trybe/camadas/projeto/spring/persistence/repository/EstudanteRepositoryImpl.java
+++ src/main/java/com/trybe/camadas/projeto/spring/persistence/repository/EstudanteRepositoryImpl.java
@@ -1,4 +1,40 @@
 package com.trybe.camadas.projeto.spring.persistence.repository;
 
+import com.trybe.camadas.projeto.spring.persistence.entity.Estudante;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EstudanteRepositoryImpl implements EstudanteRepository {
+
+  private static List<Estudante> estudantes = new ArrayList<>();
+
+  @Override
+  public Estudante criarEstudante(Estudante estudante) {
+    estudantes.add(estudante);
+    return estudante;
+  }
+
+  @Override
+  public Estudante buscaEstudantepeloId(String id) {
+    return estudantes.stream().filter(item -> item.getId().equals(id)).collect(Collectors.toList())
+        .get(0);
+  }
+
+  @Override
+  public Estudante atualizaEstudantepeloId(String id, Estudante estudante) {
+    if (estudantes.size() > 0) {
+      Estudante oldOne = buscaEstudantepeloId(id);
+      estudantes.remove(oldOne);
+      estudantes.add(estudante);
+    }
+    return estudante;
+  }
+
+  @Override
+  public void excluiEstudantepeloId(String id) {
+    Estudante estudante = buscaEstudantepeloId(id);
+    estudantes.remove(estudante);
+  }
 }
