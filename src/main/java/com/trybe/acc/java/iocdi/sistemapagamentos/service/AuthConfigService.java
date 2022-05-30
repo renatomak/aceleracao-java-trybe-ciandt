7dc3d58f4309599545c5ef06fd83e3f5fd6db2e7
renatomak <renato.mark.silva@gmail.com>
2022-05-30 18:55:55 -0300

    feat: authConfig

---
+++ src/main/java/com/trybe/acc/java/iocdi/sistemapagamentos/service/AuthConfigService.java
@@ -0,0 +1,10 @@
+package com.trybe.acc.java.iocdi.sistemapagamentos.service;
+
+import com.trybe.acc.java.iocdi.sistemapagamentos.model.Auth;
+
+public class AuthConfigService {
+
+    public Auth fazerLogin(Auth auth) {
+        return auth;
+    }
+}
