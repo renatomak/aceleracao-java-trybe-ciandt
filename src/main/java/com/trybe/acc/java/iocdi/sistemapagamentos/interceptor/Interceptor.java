671e2586d15c72e5902a301748f4febad01abb1e
renatomak <renato.mark.silva@gmail.com>
2022-05-30 18:56:22 -0300

    feat: Interceptor

---
+++ src/main/java/com/trybe/acc/java/iocdi/sistemapagamentos/interceptor/Interceptor.java
@@ -0,0 +1,27 @@
+package com.trybe.acc.java.iocdi.sistemapagamentos.interceptor;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@Component
+public class Interceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        final RestritoPermissaoEscrita restritoPessoaAdministradora = ((HandlerMethod) handler).getMethod().getAnnotation((RestritoPermissaoEscrita.class));
+
+        if (restritoPessoaAdministradora != null) {
+            response.addHeader("InterceptorTrybe", "Pessoa administradora validada pelo interceptor da trybe");
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
