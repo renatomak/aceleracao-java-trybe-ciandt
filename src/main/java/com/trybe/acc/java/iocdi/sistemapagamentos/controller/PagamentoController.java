066dc00083d75660ef33cdf89dcd92a414c4d20f
renatomak <renato.mark.silva@gmail.com>
2022-05-30 18:56:56 -0300

    feat: Pagamento controller

---
+++ src/main/java/com/trybe/acc/java/iocdi/sistemapagamentos/controller/PagamentoController.java
@@ -0,0 +1,23 @@
+package com.trybe.acc.java.iocdi.sistemapagamentos.controller;
+
+import com.trybe.acc.java.iocdi.sistemapagamentos.model.Payment;
+import com.trybe.acc.java.iocdi.sistemapagamentos.service.AuthConfigService;
+import com.trybe.acc.java.iocdi.sistemapagamentos.service.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService pagamentoService;
+
+    @PostMapping()
+    public Payment criarPagamento(@RequestBody Payment payment) {
+        return pagamentoService.criarPagamento(payment);
+    }
+
+
+}
