3d2286684892fb8c3b4637301271f04ef9510ed4
renatomak <renato.mark.silva@gmail.com>
2022-05-31 14:42:26 -0300

    feat: first commit

--- src/main/java/com/trybe/acc/java/planejadorviagens/challenge/PlanejandoDestino.java
+++ src/main/java/com/trybe/acc/java/planejadorviagens/challenge/PlanejandoDestino.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.planejadorviagens.challenge;
 
+import com.trybe.acc.java.planejadorviagens.model.Country;
 import com.trybe.acc.java.planejadorviagens.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,17 +16,39 @@ public class PlanejandoDestino {
   /**
    * Implemente a inserção.
    */
-  public void inserir() {}
+  public void inserir() {
+    countryRepository.save(new Country("BRASIL", "BRASILIA", null));
+    countryRepository.save(new Country("ARGENTINA", "BUENOS AIRES", null));
+    countryRepository.save(new Country("URUGUAI", "MONTEVIDEO", null));
+    countryRepository.save(new Country("PANAMA", "CIDADE DO PANAMA", null));
+  }
 
   /**
    * Implemente a atualização.
    */
-  public void corrigir() {}
+  public void corrigir() {
+    Country country = countryRepository.findByName("BRASIL").get(0);
+    country.setDdi("+55");
+    countryRepository.save(country);
+
+    country = countryRepository.findByName("ARGENTINA").get(0);
+    country.setDdi("+54");
+    countryRepository.save(country);
+
+    country = countryRepository.findByName("URUGUAI").get(0);
+    country.setDdi("+598");
+    countryRepository.save(country);
+
+    country = countryRepository.findByName("PANAMA").get(0);
+    country.setDdi("+507");
+    countryRepository.save(country);
+  }
 
   /**
    * Implemente a exclusão.
    */
   public void excluir() {
-
+    Country country = countryRepository.findByName("PANAMA").get(0);
+    countryRepository.delete(country);
   }
 }
