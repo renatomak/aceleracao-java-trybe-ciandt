263cfcbf36b9db55f3d2d8db675724b335e824eb
renatomak <renato.mark.silva@gmail.com>
2022-06-05 18:40:59 -0300

    feat: restaurante entity

--- src/main/java/com/trybe/restaurante/entity/RestauranteEntity.java
+++ src/main/java/com/trybe/restaurante/entity/RestauranteEntity.java
@@ -1,6 +1,52 @@
 package com.trybe.restaurante.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class RestauranteEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
+  @Column
+  private String nome;
 
+  @Column
+  private String descricao;
+
+  public RestauranteEntity(Long id, String nome, String descricao) {
+    this.id = id;
+    this.nome = nome;
+    this.descricao = descricao;
+  }
+
+  public RestauranteEntity() {}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
 }
