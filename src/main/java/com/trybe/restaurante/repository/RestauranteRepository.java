f0f40cead46d9053c13c56f9a1ee126dc15a40e8
renatomak <renato.mark.silva@gmail.com>
2022-06-05 18:43:32 -0300

    feat: restaurante repository

--- src/main/java/com/trybe/restaurante/repository/RestauranteRepository.java
+++ src/main/java/com/trybe/restaurante/repository/RestauranteRepository.java
@@ -2,10 +2,9 @@ package com.trybe.restaurante.repository;
 
 import com.trybe.restaurante.entity.RestauranteEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import javax.enterprise.context.ApplicationScoped;
 
-/**
- * Classe RestauranteRepository.
- **/
 
+@ApplicationScoped
 public class RestauranteRepository implements PanacheRepositoryBase<RestauranteEntity, Long> {
 }
