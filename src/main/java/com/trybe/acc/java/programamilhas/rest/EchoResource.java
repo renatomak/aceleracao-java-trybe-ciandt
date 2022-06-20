7923f3728600194bd680b6841862f79a0207172b
renatomak <renato.mark.silva@gmail.com>
2022-06-20 17:08:27 -0300

    feat: echo resource

---
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/EchoResource.java
@@ -0,0 +1,19 @@
+package com.trybe.acc.java.programamilhas.rest;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/echo")
+@ApplicationScoped
+public class EchoResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response echo() {
+        return Response.ok("Olá!").build();
+    }
+}
