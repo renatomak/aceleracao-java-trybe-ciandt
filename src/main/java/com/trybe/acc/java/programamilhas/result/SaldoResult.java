2fcb053b90fdeef26e266f6aeda0d29606074650
renatomak <renato.mark.silva@gmail.com>
2022-06-22 09:20:46 -0300

    fix: checkstyle

---
+++ src/main/java/com/trybe/acc/java/programamilhas/result/SaldoResult.java
@@ -0,0 +1,14 @@
+package com.trybe.acc.java.programamilhas.result;
+
+public class SaldoResult {
+
+  private Integer saldo;
+
+  public Integer getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(Integer saldo) {
+    this.saldo = saldo;
+  }
+}
