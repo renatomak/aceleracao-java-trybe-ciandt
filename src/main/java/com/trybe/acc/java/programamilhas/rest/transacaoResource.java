0c10f6e1f9bd8b8d0520dd2961e055a128a604a8
renatomak <renato.mark.silva@gmail.com>
2022-06-25 17:53:59 -0300

    feat: resource soldo and extrato

--- src/main/java/com/trybe/acc/java/programamilhas/rest/transacaoResource.java
+++ src/main/java/com/trybe/acc/java/programamilhas/rest/transacaoResource.java
@@ -1,13 +1,14 @@
 package com.trybe.acc.java.programamilhas.rest;
 
 import com.trybe.acc.java.programamilhas.dao.PessoaDao;
+import com.trybe.acc.java.programamilhas.dto.TransferenciaDto;
 import com.trybe.acc.java.programamilhas.exception.AcessoNaoAutorizadoException;
+import com.trybe.acc.java.programamilhas.service.ContaService;
 import com.trybe.acc.java.programamilhas.service.LancamentoService;
 import com.trybe.acc.java.programamilhas.util.TokenUtil;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
@@ -15,7 +16,7 @@ import javax.ws.rs.core.Response;
 
 @Path("/admin")
 @ApplicationScoped
-public class LancamentoResource {
+public class transacaoResource {
 
 
   @Inject
@@ -25,14 +26,14 @@ public class LancamentoResource {
   PessoaDao pessoaDao;
 
   @Inject
-  private LancamentoService lancamentoService;
+  private ContaService contaService;
 
   // TODO: incompleto
   @POST
-  @Path("/saldos")
-  public Response saldos(@QueryParam(value = "token") String token)
+  @Path("/transferencia")
+  public Response createTransacao(@QueryParam(value = "token") String token, TransferenciaDto dto)
           throws AcessoNaoAutorizadoException {
-    return Response.ok(lancamentoService.listSaldos(token)).build();
+    return Response.ok(contaService.getExtrato(token)).build();
   }
 
 }
