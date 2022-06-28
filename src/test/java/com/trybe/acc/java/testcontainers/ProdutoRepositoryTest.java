08c4b03380be601cf2ba84924ed1fe2f0118e0c8
renatosilva <renatosilva@ciandt.com>
2022-06-28 08:33:15 -0300

    test: produto repository

--- src/test/java/com/trybe/acc/java/testcontainers/ProdutoRepositoryTest.java
+++ src/test/java/com/trybe/acc/java/testcontainers/ProdutoRepositoryTest.java
@@ -1,15 +1,24 @@
 package com.trybe.acc.java.testcontainers;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import net.bytebuddy.utility.dispatcher.JavaDispatcher.Container;
+import com.trybe.acc.java.testcontainers.persistence.Produto;
+import com.trybe.acc.java.testcontainers.persistence.ProdutoRepository;
+
 
 @Testcontainers
 @DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
@@ -21,22 +30,51 @@ public class ProdutoRepositoryTest {
    * Por padrão, a URI utilizada é o `localhost:27017`.
    *
    */
+  @Container
+  private static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+  
+  @Autowired
+  private ProdutoRepository produtoRepository;
+  
+  private Produto produtoMock;
+  private String produtoMockId;
+  
+    
   @DynamicPropertySource
   static void mongoDbProperties(DynamicPropertyRegistry registry) {
     registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+  }
+  
+  @BeforeEach
+  void setUp() {
+    produtoMock = mockProduto();
+    produtoMockId = produtoMock.getId();
+  }
+  
+  @AfterEach
+  void cleanUp() {
+    this.produtoRepository.deleteAll();
   }
 
   @DisplayName("1 - Deve cadastrar um novo produto com sucesso.")
   @Test
   void DeveCadastrarNovoproduto() {
-    Assertions.fail("Não implementado!");
+    Produto result = this.produtoRepository.insert(produtoMock);
+    
+    Assertions.assertNotNull(result);
+    
   }
 
 
   @DisplayName("2 - Deve listar todos os produtos com sucesso.")
   @Test
   void DeveListarTodosOsprodutos() {
-    Assertions.fail("Não implementado!");
+    
+    this.produtoRepository.insert(produtoMock);
+    
+    List<Produto> result = this.produtoRepository.findAll();
+    
+    Assertions.assertNotNull(result);
   }
 
 
@@ -44,14 +82,27 @@ public class ProdutoRepositoryTest {
   @DisplayName("3 - Deve buscar um produto pelo id com sucesso.")
   @Test
   void DeveBuscarprodutoPeloId() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.insert(produtoMock);
+    
+    Produto result = this.produtoRepository.findById(produtoMockId).get();
+    
+    Assertions.assertNotNull(result);
   }
 
 
   @DisplayName("4 - Deve atualizar um produto pelo id com sucesso.")
   @Test
   void DeveAtualizarprodutoPeloId() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.insert(produtoMock);
+    
+    Produto result = this.produtoRepository.findById(produtoMockId).get();
+    result.setNome("Produto Update");
+    result.setPreco(20L);
+    
+    Produto updateMock = this.produtoRepository.save(result);
+    
+    Assertions.assertNotNull(updateMock);
+    
   }
 
 
@@ -59,7 +110,17 @@ public class ProdutoRepositoryTest {
   @DisplayName("5 - Deve excluir um produto pelo id com sucesso.")
   @Test
   void DeveExcluirprodutoPeloId() {
-    Assertions.fail("Não implementado!");
+    this.produtoRepository.insert(produtoMock);
+    
+    this.produtoRepository.deleteById(produtoMockId);
+    
+    Optional<Produto> result = this.produtoRepository.findById(produtoMockId);
+    
+    Assertions.assertEquals(result, Optional.empty()); 
+  }
+  
+  private static Produto mockProduto() {
+    return new Produto("123", "Teste01", 10L);
   }
 
 }
